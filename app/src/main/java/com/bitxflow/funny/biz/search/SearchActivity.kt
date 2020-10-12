aa86397a6b1199dd5f926d58ac7344758f4c6073
bitxflow <bitxflow@gmail.com>
2020-10-12 17:46:37 +0900

    radio group

--- app/src/main/java/com/bitxflow/funny/biz/search/SearchActivity.kt
+++ app/src/main/java/com/bitxflow/funny/biz/search/SearchActivity.kt
@@ -2,6 +2,10 @@ package com.bitxflow.funny.biz.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import com.bitxflow.funny.R
 import kotlinx.android.synthetic.main.activity_search.*
 import java.util.ArrayList
@@ -13,19 +17,35 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        var ppl = 0;
+
         val game = Game()
         game.title = "abcd"
 
         gameList.add(game)
 
         val game2 = Game()
-        game2.title = "abcd"
+        game2.title = "efg"
 
         gameList.add(game2)
 
         val adapter = GameListAdapter(applicationContext , gameList )
         adapter.notifyDataSetChanged()
         game_listview.adapter = adapter
+
+
+
+        search_bt.setOnClickListener {
+//            val id = ppl_rg.checkedRadioButtonId
+//            val radioButton = (RadioButton)findViewById(id)
+            when(ppl_rg.checkedRadioButtonId){
+                R.id.ppl2_radio_bt -> ppl=2
+                R.id.ppl3_radio_bt -> ppl=3
+                R.id.ppl4_radio_bt -> ppl=4
+            }
+
+            Toast.makeText(applicationContext,"ppl?:" + ppl,Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
