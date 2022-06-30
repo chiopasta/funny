dc9b32d9d28dabd1c3552c3844dfb9f05ef9240a
chiopasta <iacolytei@gmail.com>
2022-06-30 22:11:13 +0900

    sncak 길게 바꿈

--- app/src/main/java/com/bitxflow/funny/biz/snack/SnackActivity.kt
+++ app/src/main/java/com/bitxflow/funny/biz/snack/SnackActivity.kt
@@ -3,16 +3,16 @@ package com.bitxflow.funny.biz.snack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bitxflow.funny.R
-import kotlinx.android.synthetic.main.activity_snack.*
+import kotlinx.android.synthetic.main.activity_snack_long.*
 
 class SnackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_snack)
+        setContentView(R.layout.activity_snack_long)
 
-        val pagerAdapter = ViewPagerAdapter(this)
-        snack_vp.adapter = pagerAdapter
-        indicator.setViewPager(snack_vp)
+//        val pagerAdapter = ViewPagerAdapter(this)
+//        snack_vp.adapter = pagerAdapter
+//        indicator.setViewPager(snack_vp)
 
         snack_backpress_bt.setOnClickListener {
             finish()
